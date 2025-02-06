96dedc21d232cb2bfcf58bb40def0f4781e80018
ruchita3107 <rruucchhii31@gmail.com>
2025-02-06 14:48:54 -0600

    Create tomcatdeploy.groovy

---
+++ src/main/webapp/tomcatdeploy.groovy
@@ -0,0 +1,4 @@
+sshagent(["${credId}}) {
+   sh "scp target/${warName}.war ec2-user@${ip}:/opt/tomcat10/webapps" 
+   sh "ssh ec2-user@${ip} /opt/tomcat10/bin/shutdown.sh"
+   sh "ssh ec2-user@${ip} /opt/tomcat10/bin/startup.sh" 
